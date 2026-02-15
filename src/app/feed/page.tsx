a33b8b122f1bcc67af34aac6987e8206c50495fc
Psyberly Labs <psyberlylabs@gmail.com>
2026-02-15 08:38:53 +0000

    when i click the top or down arrow it should go to the next so if i slid

--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -5,12 +5,10 @@ import { videoPosts } from '@/lib/data';
 export default function FeedPage() {
   return (
     <AppLayout>
-      <div className="h-full w-full overflow-y-auto snap-y snap-mandatory">
-        <div className="max-w-md mx-auto">
-          {videoPosts.map((post) => (
-            <VideoPost key={post.id} post={post} />
-          ))}
-        </div>
+      <div className="h-full w-full max-w-md mx-auto overflow-y-auto snap-y snap-mandatory">
+        {videoPosts.map((post) => (
+          <VideoPost key={post.id} post={post} />
+        ))}
       </div>
     </AppLayout>
   );
